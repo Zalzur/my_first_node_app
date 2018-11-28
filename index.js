087787e13a9b73531513e52976899868ac688ab6
Oláh Tamás <tolah94@gmail.com>
2018-11-28 10:33:13 +0100

    Implemented /helloworld route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/helloworld', function (req, res) {
+    res.send({message: 'ezigen'});
+});
+
 app.listen(3000, function () {
     console.log('The app listening on port 3000!');
 });
