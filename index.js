8d793b032f70b7d0fb17c53b22202f7b5ef9b0f9
Oláh Tamás <tolah94@gmail.com>
2018-11-28 10:38:18 +0100

    Implemented /secondroute route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/helloworld', function (req, res) {
     res.send({message: 'ezigen'});
 });
 
+app.get('/secondroute', function (req, res) {
+    res.send({message: 'success'});
+});
+
 app.listen(3000, function () {
     console.log('The app listening on port 3000!');
 });
